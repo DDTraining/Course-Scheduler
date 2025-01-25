2fd951c031baaee6e83514c44ff10e01a68cba7c
DDTraining <daniel.ikponmwen@gmail.com>
2025-01-24 18:47:18 -0700

    added course schedules and updated App function

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,54 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Banner from './components/Banner'
+import CourseList from './components/CourseList';
+
+const schedule = {
+  "title": "CS Courses for 2020-2021",
+  "courses": {
+    "F101": {
+      "term": "Fall",
+      "number": "101",
+      "meets": "MWF 11:00-11:50",
+      "title": "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110": {
+      "term": "Fall",
+      "number": "110",
+      "meets": "MWF 10:00-10:50",
+      "title": "Intro Programming for non-majors"
+    },
+    "S313": {
+      "term": "Spring",
+      "number": "313",
+      "meets": "TuTh 15:30-16:50",
+      "title": "Tangible Interaction Design and Learning"
+    },
+    "S314": {
+      "term": "Spring",
+      "number": "314",
+      "meets": "TuTh 9:30-10:50",
+      "title": "Tech & Human Interaction"
+    }
+  }
+};
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Banner title={schedule.title} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <CourseList courses={schedule.courses} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
